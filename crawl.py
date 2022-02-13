f8d4a83b1e616a45b525db273cc77c415e2d0e21
Dif71 <93062302+Dif71@users.noreply.github.com>
2022-02-13 15:39:22 +0700

    Add files via upload

---
+++ crawl.py
@@ -0,0 +1,271 @@
+import os,subprocess,datetime,time,random,argparse,http.client,json,zipfile,pyautogui,requests,urllib.parse,lib
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium_stealth import stealth
+from random import randint,shuffle
+
+
+# Initialize parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--Target", required=True)
+parser.add_argument("-p", "--Proxy")
+parser.add_argument("-r", "--Referer")
+parser.add_argument("-v", "--Version")
+args = parser.parse_args()
+
+
+if args:
+      
+    Target = "http://" + args.Target
+    Query = "site:" + args.Target
+    Version = args.Version
+    Referer = args.Referer
+    
+    # if Version:
+        # vR = args.Version
+    # else:
+        # vR = "desktop"
+    # PDIR = "/root/x/p/"+vR
+    # if not os.path.exists(PDIR):
+      # os.makedirs(PDIR)
+    
+    ReferX = ["https://www.google.com/search?q="+Query+"&hl=en&gl=US","https://www.bing.com/search?q="+Query+"&FORM=HDRSC1","https://www.baidu.com/s?wd="+Query,]
+    # RefX = ReferX[1]
+    # random.shuffle(ReferX)
+    if Referer == "google":
+        RefX = "https://www.google.com/search?q="+Query+"&hl=en&gl=US"
+    elif Referer == "baidu":
+        RefX = "https://www.baidu.com/s?wd="+Query
+    elif Referer == "bing":
+        RefX = "https://www.bing.com/search?q="+Query
+    elif Referer == "bing-ecosia":
+        RefX = "https://www.ecosia.org/search?&q="+Query
+    elif Referer == "duckduckgo":
+        RefX = "https://duckduckgo.com/?q="+Query
+    elif Referer == "yandex":
+        RefX = "https://yandex.com/search/?text="+Query
+    elif Referer == "yahoo":
+        RefX = "https://search.yahoo.com/search?p="+Query
+    else:
+        RefX = "https://www.google.com/search?q="+Query+"&hl=en&gl=US"
+        
+    
+    ua = lib.ua(Version)
+    
+    if "Android"  in ua or "iPhone"  in ua or "iPod" in ua:
+        h = [240,360,480,480]
+        w = [320,640,640,800]
+    elif "iPad" in ua:
+        h = [768,864,768,600,600,480,480]
+        w = [1366,1152,1024,1024,800,800,640]
+    else :
+        w = [1024,1280,1280,1280,1360,1440,1152,1600,1600,1680,1920,1920]
+        h = [768,720,800,1024,768,900,864,900,1200,1050,1080,1200]
+        
+    t = random.randint(0,len(w)-1)
+    
+    subprocess.call(["xrandr", "--fb", str(w[t])+ "x"+str(h[t])])
+    # subprocess.call(["xrandr", "--mode", str(w[t])+ "x"+str(h[t])],"--display",":0")
+    time.sleep(5)
+    
+    if "Mac" in ua:
+        VendorX = "Apple Computer, Inc."
+    else:
+        VendorX = "Google Inc."
+    
+    if "Windows" in ua:
+        if "Win64" in ua:
+            PlatformX = "Win64"
+        elif "Windows" in ua:
+            PlatformX = "Win32"
+        else:
+            PlatformX = "Windows"
+    elif "Mac" in ua:
+        PlatformX = "Macintosh"
+    else:
+        PlatformX = "Linux"
+        
+    def ad_popup(browser):
+        try:
+            WebDriverWait(browser,5).until(EC.visibility_of_element_located((By.CLASS_NAME, "coupon-poplayer-modal"))) ##coupon popup
+            WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "btn-close"))).click()
+        except:
+            pass
+    
+    def mose(X,Y):
+        pX = random.randint(100,X-100)
+        pY = random.randint(100,Y-200)
+        pyautogui.moveTo(pX, pY, random.randint(1,3), pyautogui.easeInOutQuad)
+    
+    def skrol():
+        tr = random.randint(1,3)
+        time.sleep(tr)
+        if tr==2:
+            tuen = stop//random.randint(1,10)
+            browser.execute_script("scrollBy(0,-"+str(tuen)+");")                
+        elif tr==3:
+            tuen = stop//random.randint(1,10)
+            browser.execute_script("scrollBy(0,-"+str(tuen)+");")
+            tuen = stop//random.randint(1,10)
+            browser.execute_script("scrollBy(0,"+str(tuen)+");")
+        else:
+            pyautogui.moveTo(random.randint(0,w[t]), random.randint(0,h[t]), 1, pyautogui.easeInOutQuad)
+
+    
+    def muter():
+        link = browser.find_elements(By.XPATH,"//a[@href]")
+        random.shuffle(link)
+        for a in link:
+            if Target in a.get_attribute("href"):
+                # a.click()
+                # try:
+                
+                # panelHeight = browser.execute_script('return window.outerHeight – window.innerHeight;')
+                # location = a.location
+                # size = a.size
+                # bezier_mouse(location, size, panelHeight)
+                
+                pyautogui.moveTo(a.location['x'],a.location['y'],5, pyautogui.easeInQuad)
+                time.sleep(random.randint(1,2))
+                action = ActionChains(browser)
+                action.move_to_element(a).click().perform()
+                # except:
+                    # browser.refresh()
+                break
+    
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # options.add_argument("--disable-extensions")
+    # options.add_argument("--profile-directory=/root/x/p")
+    # options.add_argument("--ignore-certificate-errors")
+    # options.add_argument("--disable-plugins-discovery")
+    options.add_argument("--incognito")
+    if args.Proxy:
+        PROXY = args.Proxy
+        ipx = PROXY.split(':')
+        if PROXY.count(":")==3:
+            p_json = lib.proxy_json()
+            p_js = lib.proxy_js(ipx[0],ipx[1],ipx[2],ipx[3])
+            pluginfile = 'proxy.zip'
+            with zipfile.ZipFile(pluginfile, 'w') as zp:
+                zp.writestr("manifest.json", p_json)
+                zp.writestr("background.js", p_js)
+            options.add_extension(pluginfile)
+        else:
+            options.add_argument('--proxy-server='+PROXY)
+        ipi = requests.get("http://worldtimeapi.org/api/ip/"+ipx[0]).json()
+        tz_params = {'timezoneId': ipi['timezone']}
+    
+    # options.add_argument("user-data-dir="+PDIR)
+    options.add_argument("--disable-fre")
+    options.add_argument("--no-default-browser-check")
+    options.add_argument("--no-no-first-run")
+    options.add_argument("disable-popup-blocking")
+    options.add_argument("disable-notifications")
+    options.add_argument("disable-gpu")
+    options.add_argument("--window-size="+str(w[t])+","+str(h[t]))
+    options.add_argument('user-agent=%s' % ua)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    
+    browser = webdriver.Chrome(options=options)
+    browser.delete_all_cookies()
+    if args.Proxy:
+        browser.execute_cdp_cmd('Emulation.setTimezoneOverride', tz_params)
+    stealth(browser,languages=["en-US", "en"],vendor=VendorX,platform=PlatformX,webgl_vendor="Intel Inc.",renderer="Intel Iris OpenGL Engine",fix_hairline=True,)
+    # browser.get(Target)
+    # browser.maximize_window()
+    # browser.switch_to_active_element()
+    # browser.set_window_position(0, 0)
+    # browser.set_window_size(w[t], h[t])
+    # browser.get(RefX)
+
+    # browser.get("https://whoer.net/")
+    # browser.get("https://2ip.io/privacy/")
+    # browser.get("https://pixelscan.net/")
+    # time.sleep(10)
+    
+    
+    browser.get("https://www.google.com/?hl=en")
+    # ad_popup(browser)
+    
+    
+    try :
+    
+        if Version == "mobile" :
+            scroll = WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, '[class="section-layout section-scrollbox scrollable-y scrollable-show"]')))
+            browser.execute_script("arguments[0].scrollBy(0,arguments[0].scrollHeight)", scroll)
+    
+        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#L2AGLb > div")))
+        browser.find_element(By.CSS_SELECTOR, "#L2AGLb > div").click()
+        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="q"]')))
+    except:
+        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="q"]')))
+    
+    q = browser.find_element(By.CSS_SELECTOR, 'input[name="q"]')
+    q.send_keys(Query)
+    q.send_keys(Keys.ENTER)
+    
+    time.sleep(5)
+    
+    
+    # browser.get(RefX)
+    # time.sleep(5)
+    
+    
+    # pyautogui.moveTo(random.randint(0,w[t]), random.randint(0,h[t]), 1, pyautogui.easeInOutQuad)
+    begin_time = datetime.datetime.now()
+    try :
+        ################## PAGE VIEW ##################
+        for i in range(random.randint(3,7)):
+            if w[t]>1000:
+                for j in range(1,2):
+                    mose(w[t],h[t])
+            muter()
+            cek = browser.find_elements(By.CLASS_NAME, 'list-item')
+            if cek :
+                stop = random.randint((h[t]*2),(h[t]*10))
+            else :
+                stop = random.randint(h[t],(h[t]*2))
+
+            total_height = int(browser.execute_script("return document.body.scrollHeight"))
+            dly = random.randint(75,200)
+            for j in range(1, total_height):
+                browser.execute_script("window.scrollTo(0, {});".format(j))
+                if j%dly == 0:
+                    time.sleep(0.3)
+                if j == stop :
+                    break
+            skrol()
+            time.sleep(random.randint(2,4))
+
+        ################## CLICK ADS ##################
+        link = browser.find_elements(By.XPATH,"//a[@href]")
+        random.shuffle(link)
+        for a in link:
+            # if 'googleads.g.doubleclick.net' in a.get_attribute("href"):
+            if 'about' in a.get_attribute("href"):
+                a.click()
+                print("sukses")
+                break
+                dly = random.randint(75,200)
+                total_height = int(browser.execute_script("return document.body.scrollHeight"))
+                for j in range(1, total_height):
+                    browser.execute_script("window.scrollTo(0, {});".format(j))
+                    if j%dly == 0:
+                        time.sleep(0.5)
+                skrol()
+                time.sleep(random.randint(5,15))
+        browser.quit()
+    except:
+        print("error")
+        # subprocess.call(["python3.8","crawl.py","-t",Target,"-r",Referer])
+        browser.quit()
+    print(datetime.datetime.now()-begin_time)
+
+    
---
+++ lib.py
@@ -0,0 +1,142 @@
+import random
+import numpy as np
+import bezier
+
+def proxy():
+    prxs = ['','']
+    random.shuffle(prxs)
+    PROXY = uas[0]
+    return PROXY
+
+def ua(Version):
+    if Version=="mobile":
+        uas = ["Mozilla/5.0 (iPad; CPU OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/98.0.4758.85 Mobile/15E148 Safari/604.1","Mozilla/5.0 (iPod; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/98.0.4758.85 Mobile/15E148 Safari/604.1","Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 10; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 10; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 10; SM-N960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 10; LM-Q720) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 10; LM-X420) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 10; LM-Q710(FGN)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 12; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 12; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 12; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 12; SM-N960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 12; LM-Q720) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 12; LM-X420) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 12; LM-Q710(FGN)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36","Mozilla/5.0 (Android 12; Mobile; rv:68.0) Gecko/68.0 Firefox/96.0","Mozilla/5.0 (Android 12; Mobile; LG-M255; rv:96.0) Gecko/96.0 Firefox/96.0","Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1","Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/98.0.4758.85 Mobile/15E148 Safari/604.1","Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/96.0 Mobile/15E148 Safari/605.1.15","Mozilla/5.0 (iPad; CPU OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1","Mozilla/5.0 (iPod touch; CPU iPhone 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1","Mozilla/5.0 (Linux; Android 10; VOG-L29) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36 OPR/63.3.3216.58675","Mozilla/5.0 (Linux; Android 10; SM-G970F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36 OPR/63.3.3216.58675","Mozilla/5.0 (Linux; Android 10; SM-N975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36 OPR/63.3.3216.58675","Mozilla/5.0 (Linux; Android 10; HD1913) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36 EdgA/97.0.1072.69","Mozilla/5.0 (Linux; Android 10; SM-G973F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36 EdgA/97.0.1072.69","Mozilla/5.0 (Linux; Android 10; Pixel 3 XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36 EdgA/97.0.1072.69","Mozilla/5.0 (Linux; Android 10; ONEPLUS A6003) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Mobile Safari/537.36 EdgA/97.0.1072.69","Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 EdgiOS/97.1072.69 Mobile/15E148 Safari/605.1.15","Mozilla/5.0 (Windows Mobile 10; Android 10.0; Microsoft; Lumia 950XL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Mobile Safari/537.36 Edge/40.15254.603","Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/96.0 Mobile/15E148 Safari/605.1.15","Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/96.0 Mobile/15E148 Safari/605.1.15","Mozilla/5.0 (iPod touch; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/604.5.6 (KHTML, like Gecko) FxiOS/96.0 Mobile/15E148 Safari/605.1.15"]
+    else :
+        uas = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36","Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0","Mozilla/5.0 (Macintosh; Intel Mac OS X 12.2; rv:96.0) Gecko/20100101 Firefox/96.0","Mozilla/5.0 (X11; Linux i686; rv:96.0) Gecko/20100101 Firefox/96.0","Mozilla/5.0 (Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0","Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:96.0) Gecko/20100101 Firefox/96.0","Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0","Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (Macintosh; Intel Mac OS X 12.2; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (X11; Linux i686; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0","Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)","Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)","Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)","Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)","Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)","Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)","Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)","Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko","Mozilla/5.0 (Windows NT 6.2; Trident/7.0; rv:11.0) like Gecko","Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko","Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/97.0.1072.69","Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/97.0.1072.69","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 OPR/83.0.4254.16","Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 OPR/83.0.4254.16","Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 OPR/83.0.4254.16","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 OPR/83.0.4254.16","Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15","Mozilla/5.0 (X11; CrOS x86_64 14268.67.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.111 Safari/537.36","Mozilla/5.0 (X11; CrOS armv7l 14268.67.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.111 Safari/537.36","Mozilla/5.0 (X11; CrOS aarch64 14268.67.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.111 Safari/537.36","Mozilla/5.0 (X11; CrOS x86_64 14268.67.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.111 Safari/537.36","Mozilla/5.0 (X11; CrOS armv7l 14268.67.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.111 Safari/537.36","Mozilla/5.0 (X11; CrOS aarch64 14268.67.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.111 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15","Mozilla/5.0 (Macintosh; Intel Mac OS X 12.2; rv:96.0) Gecko/20100101 Firefox/96.0","Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Vivaldi/4.3","Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/97.0.1072.69","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/97.0.1072.69","Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36","Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Vivaldi/4.3","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Vivaldi/4.3"]
+    random.shuffle(uas)
+    UA = uas[0]
+    return UA
+
+def bezier_mouse(location, size, panelHeight): ##move mouse to middle of element
+    x, relY = location["x"], location["y"]
+    absY = relY + panelHeight
+    w, h = size["width"], size["height"]
+    wCenter = w/2
+    hCenter = h/2
+    xCenter = int(wCenter + x)
+    yCenter = int(hCenter + absY)
+
+    start = pyautogui.position()
+    end = xCenter, yCenter
+
+    x2 = (start[0] + end[0])/2 #midpoint x
+    y2 = (start[1] + end[1]) / 2 ##midpoint y
+
+    control1X = (start[0] + x2)/2
+    control1Y = (end[1] + y2) / 2
+
+    control2X = (end[0] + x2) / 2
+    control2Y = (start[1] + y2) / 2
+
+    # Two intermediate control points that may be adjusted to modify the curve.
+    control1 = control1X, y2 ##combine midpoints to creat perfect curve
+    control2 = control2X, y2
+
+
+    # Format points to use with bezier
+    control_points = np.array([start, control1, control2, end])
+    points = np.array([control_points[:, 0], control_points[:, 1]])  # Split x and y coordinates
+    # You can set the degree of the curve here, should be less than # of control points
+    degree = 3
+    # Create the bezier curve
+    curve = bezier.Curve(points, degree)
+
+    curve_steps = 50  # How many points the curve should be split into. Each is a separate pyautogui.moveTo() execution
+    delay = 0.003  # Time between movements. 1/curve_steps = 1 second for entire curve
+
+    # Move the mouse
+    for j in range(1, curve_steps + 1):
+        # The evaluate method takes a float from [0.0, 1.0] and returns the coordinates at that point in the curve
+        # Another way of thinking about it is that i/steps gets the coordinates at (100*i/steps) percent into the curve
+        x, y = curve.evaluate(j / curve_steps)
+        pyautogui.moveTo(x, y)  # Move to point in curve
+        pyautogui.sleep(delay)  # Wait delay
+
+def proxy_json():
+    manifest_json = """
+    {
+    "version": "1.0.0",
+    "manifest_version": 2,
+    "name": "Chrome Proxy",
+    "permissions": [
+        "proxy",
+        "tabs",
+        "unlimitedStorage",
+        "storage",
+        "<all_urls>",
+        "webRequest",
+        "webRequestBlocking"
+    ],
+    "background": {
+        "scripts": ["background.js"]
+    },
+    "minimum_chrome_version":"22.0.0"
+    }
+    """
+    return manifest_json
+    
+def proxy_js(PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS):
+    background_js = """
+    var config = {
+        mode: "fixed_servers",
+        rules: {
+        singleProxy: {
+            scheme: "http",
+            host: "%s",
+            port: parseInt(%s)
+        },
+        bypassList: ["localhost"]
+        }
+    };
+
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+    function callbackFn(details) {
+        return {
+            authCredentials: {
+            username: "%s",
+            password: "%s"
+            }
+        };
+    }
+
+    chrome.webRequest.onAuthRequired.addListener(
+            callbackFn,
+            {urls: ["<all_urls>"]},
+            ['blocking']
+    );
+    """ % (PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS)
+    return background_js
+        
+        
+        
+        
+        
+        
+
+"""
+def roxy():
+    from fp.fp import FreeProxy
+    proxy = FreeProxy(timeout=0.1).get()
+    proxy = proxy.replace("http://","")
+    print(proxy)
+def proxy():
+    key = '3MHXi8EN330DSWbR0GJGzkPW5vFLDMgX'
+    conn = http.client.HTTPSConnection("api.webscrapingapi.com")
+    conn.request("GET", "/v1?api_key=" + key + "&url=https%3A%2F%2Fapi.ipify.org%2F%3Fformat%3Djson&device=desktop&proxy_type=residential")
+    res = conn.getresponse()
+    data = res.read()
+    ips = data.decode("utf-8")
+    jip = json.loads(ips)
+    print(jip['ip'] + ":55443")
+"""
